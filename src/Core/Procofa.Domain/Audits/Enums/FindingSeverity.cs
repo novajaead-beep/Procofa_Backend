3b85311a9e8b17e1b26ed1512995ab2854140594
novajae <novajaead@gmail.com>
2026-08-23 16:01:14 -0600

    Delete src directory

--- src/Core/Procofa.Domain/Audits/Enums/FindingSeverity.cs
+++
@@ -1,9 +0,0 @@
-namespace Procofa.Domain.Audits.Enums;
-
-/// <summary>Clasificación de severidad de una no conformidad (HU-16).</summary>
-public enum FindingSeverity
-{
-    Minor = 1,
-    Major = 2,
-    Critical = 3
-}
